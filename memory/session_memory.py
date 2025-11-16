2c191b033d46894e19d29253de96ddec43fc0f35
Komal Harshita <komal.sony234@gmail.com>
2025-11-16 13:30:01 +0530

    Implement SessionMemory class for message history

---
+++ memory/session_memory.py
@@ -0,0 +1,9 @@
+class SessionMemory:
+    def __init__(self):
+        self.history = []
+
+    def add_history(self, sender, message):
+        self.history.append({"sender": sender, "message": message})
+
+    def get_history(self):
+        return self.history
