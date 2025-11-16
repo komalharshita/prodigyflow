c199132a7b93c820813bc8f5a0f00a2443b5824f
Komal Harshita <komal.sony234@gmail.com>
2025-11-16 13:27:38 +0530

    remove old files

--- tools/course_loader.py
+++
@@ -1,3 +0,0 @@
-class CourseLoader:
-  def load_course_outline(self, course_name):
-    return f"Loaded outline for {course_name}"
--- tools/resource_fetcher.py
+++
@@ -1,3 +0,0 @@
-class ResourceFetcher:
-def fetch_resources(self, topic):
-return [f"Resource 1 for {topic}", f"Resource 2 for {topic}"]
